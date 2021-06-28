ff66cd4fff04c28c1f94ad803b658a8a87c2d336
Gobinath24 <gobinathvb24@gmail.com>
2021-06-28 09:13:54 -0700

    using increment and decrement operation

--- src/feature/reducer/Reducer.jsx
+++ src/feature/reducer/Reducer.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { Button } from 'semantic-ui-react';
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from './testReducer';
 
 export default function Reducer() {
+  const dispatch = useDispatch();
   const data = useSelector(state => state.data)
 
   return (
     <>
      <h2>Testing 123</h2>
      <h4>The data is: {data}</h4>
+     <Button onClick={() => dispatch({type: INCREMENT_COUNTER})} content='Increment' color='green' />
+     <Button onClick={() => dispatch({type: DECREMENT_COUNTER})} content='Decrement' color='red' />
     </>
   )
 }
